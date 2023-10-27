f2a87ff05d0e5b5fa0bb9f26398826a98637fb48
Paco Florido <franciscoflorido0@gmail.com>
2023-10-28 00:02:15 +0200

    Punto 18

--- app/src/main/java/net/iesochoa/pacofloridoquesada/practica3/MainActivity.kt
+++ app/src/main/java/net/iesochoa/pacofloridoquesada/practica3/MainActivity.kt
@@ -2,14 +2,17 @@ package net.iesochoa.pacofloridoquesada.practica3
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import net.iesochoa.pacofloridoquesada.practica3.databinding.ActivityMainBinding
 
+private const val TAG = "practica3"
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     var num:Int = 0;
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         //setContentView(R.layout.activity_main)
+
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
@@ -18,5 +21,31 @@ class MainActivity : AppCompatActivity() {
             num++
             binding.tvNumero.text = num.toString()
         }
+
+        Log.i(TAG,"onCreate")
+    }
+    override fun onStart() {
+        super.onStart()
+        Log.i(TAG, "onStart")
+    }
+    override fun onResume() {
+        super.onResume()
+        Log.i(TAG, "onResume")
+    }
+    override fun onPause() {
+        Log.i(TAG, "onPause")
+        super.onPause()
+    }
+    override fun onStop() {
+        Log.i(TAG, "onStop")
+        super.onStop()
+    }
+    override fun onRestart() {
+        super.onRestart()
+        Log.i(TAG, "onRestart")
+    }
+    override fun onDestroy() {
+        Log.i(TAG, "onDestroy")
+        super.onDestroy()
     }
 }
